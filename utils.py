9b070f8ec8bfaabe582738e1199d2f726558d674
Ren Tatsumoto <tatsu@autistici.org>
2024-04-15 19:42:35 +0300

    add utils

---
+++ utils.py
@@ -0,0 +1,5 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+def ui_translate(key: str) -> str:
+    return key.capitalize().replace("_", " ")
