580a77bb4ee1330503acad816b03cca799760c65
Ren Tatsumoto <tatsu@autistici.org>
2025-05-12 21:19:49 +0300

    add function relevant_field_names()
    move function from AJT Japanese

--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,7 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import typing
-from collections.abc import Iterable
+from typing import Optional, Iterable
 
 from aqt import mw
 
@@ -34,9 +34,17 @@ def get_model_field_names(model_dict: typing.Optional[AnkiNoteTypeDict]) -> Iter
     return (field["name"] for field in model_dict["flds"])
 
 
+def relevant_field_names(note_type_name_fuzzy: Optional[str] = None) -> Iterable[str]:
+    """
+    Return an iterable of field names present in note types whose names contain the first parameter.
+    """
+    for model in mw.col.models.all_names_and_ids():
+        if not note_type_name_fuzzy or note_type_name_fuzzy.lower() in model.name.lower():
+            yield from get_model_field_names(mw.col.models.get(model.id))
+
+
 def gather_all_field_names() -> Iterable[str]:
     """
     Returns all field names found in all note types found in the collection.
     """
-    for model in mw.col.models.all_names_and_ids():
-        yield from get_model_field_names(mw.col.models.get(model.id))
+    return relevant_field_names()
