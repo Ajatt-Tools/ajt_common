f3ec82b1bcf938a62e13fd3faf336e0a3fa9ae0a
Ren Tatsumoto <tatsu@autistici.org>
2025-05-12 20:59:05 +0300

    add class EditableSelector
    move this class from AJT Japanese

--- anki_field_selector.py
+++ anki_field_selector.py
@@ -1,21 +1,30 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
 from aqt import mw
 from aqt.qt import *
+from PyQt6.QtWidgets import QComboBox
 
 from .model_utils import gather_all_field_names
 
 
-class AnkiFieldSelector(QComboBox):
+class EditableSelector(QComboBox):
+    """
+    Convenience class for making combo boxes with editable input field.
+    """
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.setEditable(True)
+
+
+class AnkiFieldSelector(EditableSelector):
     """
     An editable combobox prepopulated with all field names
     present in Note Types in the Anki collection.
     """
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.setEditable(True)
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
         try:
             self.addItems(dict.fromkeys(gather_all_field_names()))
         except AttributeError:
