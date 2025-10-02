45df1e5ad46327bb754ef3a392f2f4404283fdd2
Ren Tatsumoto <tatsu@autistici.org>
2025-10-03 01:19:23 +0300

    update format.sh

--- format.sh
+++ format.sh
@@ -1,19 +1,77 @@
 #!/bin/bash
 
+set -euo pipefail
+
 echo "Formatting $PWD"
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 readonly ROOT_DIR
 
-# Pass a list of files or take all files in the repository.
+EXCLUDED=()
+INCLUDED=()
 
-if [[ $# -gt 0 ]]; then
-	FILES=("$@")
-else
-	readarray -t FILES <<<"$(find "$ROOT_DIR" -iname '*.py')"
-fi
-readonly -a FILES
+read_cmd_args() {
+	local fp
+	while (($# > 0)); do
+		case $1 in
+		-e|--exclude)
+			shift
+			fp="${ROOT_DIR%%/}/${1%%/}"
+			EXCLUDED+=("$fp")
+			echo "exclude $fp"
+			;;
+		-i|--include)
+			shift
+			fp="${ROOT_DIR%%/}/${1%%/}"
+			INCLUDED+=("$fp")
+			echo "include $fp"
+			;;
+		"")
+			break
+			;;
+		*)
+			echo "Unknown command: '$1'"
+			exit 1
+			;;
+		esac
+		shift
+	done
+	readonly -a EXCLUDED INCLUDED
+}
 
-pyupgrade --py39-plus "${FILES[@]}"
-isort "${FILES[@]}"
-black --line-length 120 --target-version py39 "${FILES[@]}"
+is_excluded() {
+	local file=$1
+	for entry in "${EXCLUDED[@]}"; do
+		if [[ ${file} == "${entry}" ]] || [[ ${file} == "${entry}"/* ]]; then
+			return 0
+		fi
+	done
+	return 1
+}
+
+main() {
+	TO_FORMAT=()
+
+	if (($# == 0)); then
+		readarray -t TO_FORMAT <<<"$(find "$ROOT_DIR" -iname '*.py')"
+		readonly -a TO_FORMAT
+	else
+		read_cmd_args "$@"
+		readarray -t FILES <<<"$(find "${INCLUDED[@]}" -iname '*.py')"
+		readonly -a FILES
+
+		for file in "${FILES[@]}"; do
+			if is_excluded "$file"; then
+				echo "excluded: $file"
+			else
+				TO_FORMAT+=("$file")
+			fi
+		done
+	fi
+	readonly -a TO_FORMAT
+	pyupgrade --py39-plus "${TO_FORMAT[@]}"
+	isort "${TO_FORMAT[@]}"
+	black --line-length 120 --target-version py39 "${TO_FORMAT[@]}"
+}
+
+main "$@"
