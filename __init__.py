a3677752d1da9c6eaf0a225673de9a0dbdcd0903
Ren Tatsumoto <tatsu@autistici.org>
2026-03-18 22:38:24 +0300

    do nothing if anki is not installed

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org> and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from anki.utils import int_version
-
-if int_version() < 231000:
-    raise RuntimeError("This Anki version is not supported.")
+try:
+    from anki.utils import int_version
+except ImportError:
+    pass
+else:
+    if int_version() < 231000:
+        raise RuntimeError("This Anki version is not supported.")
