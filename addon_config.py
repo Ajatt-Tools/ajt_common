ed3d04f03f17c03de72d8c90ccf6f70d41bd6979
Ren Tatsumoto <tatsu@autistici.org>
2026-06-23 03:02:57 +0300

    change copyright

--- addon_config.py
+++ addon_config.py
@@ -1,5 +1,6 @@
-# Copyright: Ren Tatsumoto <tatsu at autistici.org>
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 import abc
 from collections.abc import Iterable
 from typing import Any, Callable, Optional, cast
